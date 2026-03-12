e83739c387427b1227b36db08412530c6b8b8360
aribilgiogr <aribilgiogr@gmail.com>
2026-03-12 12:56:23 +0300

    Update 01 12.03.2026

--- Business/Services/IdentityConfig.cs
+++ Business/Services/IdentityConfig.cs
@@ -1,18 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using System.Web;
+﻿using Core.Concrete.Entities;
+using Data.Contexts;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
-using UI.Models;
+using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
-namespace UI
+
+namespace Business.Services
 {
     public class EmailService : IIdentityMessageService
     {
---
+++ Data/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Core.Concrete.Entities;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Data.Contexts
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext() : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
