c012eef9ab76a3366dfa4bda401e7e021644b20e
aribilgiogr <aribilgiogr@gmail.com>
2026-03-16 14:36:29 +0300

    Update 02 16.03.2026

---
+++ Core/Abstracts/Bases/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Core.Abstracts.Bases
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public bool Active { get; set; } = true;
+        public bool Deleted { get; set; } = false;
+    }
+}
